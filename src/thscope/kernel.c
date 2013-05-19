ad6dbb483aade838b0861280123b9f1de8d166a5
Olli Vanhoja <olli.vanhoja@ninjaware.fi>
2013-05-19 21:34:08 +0300

    hmm... it seems that binary size gets smaller if all syscall wrappers are declared in one file.
    Signed-off-by: Olli Vanhoja <olli.vanhoja@ninjaware.fi>

--- src/thscope/kernel.c
+++ src/thscope/kernel.c
@@ -33,6 +33,259 @@ void osGetLoadAvg(uint32_t loads[3])
     syscall(KERNEL_SYSCALL_SCHED_GET_LOADAVG, loads);
 }
 
+/** @addtogroup Thread_Management
+  * @{
+  */
+
+osThreadId osThreadCreate(osThreadDef_t * thread_def, void * argument)
+{
+    ds_osThreadCreate_t args;
+    osThreadId result;
+
+    args.def = thread_def;
+    args.argument = argument;
+    result = (osThreadId)syscall(KERNEL_SYSCALL_SCHED_THREAD_CREATE, &args);
+
+    /* Request immediate context switch */
+    req_context_switch();
+
+    return result;
+}
+
+osThreadId osThreadGetId(void)
+{
+    osThreadId result;
+
+    result = (osThreadId)syscall(KERNEL_SYSCALL_SCHED_THREAD_GETID, NULL);
+
+    return result;
+}
+
+osStatus osThreadTerminate(osThreadId thread_id)
+{
+    osStatus result;
+
+    result = (osStatus)syscall(KERNEL_SYSCALL_SCHED_THREAD_TERMINATE, &thread_id);
+
+    return result;
+}
+
+osStatus osThreadYield(void)
+{
+    /* Request immediate context switch */
+    req_context_switch();
+
+    return (osStatus)osOK;
+}
+
+osStatus osThreadSetPriority(osThreadId thread_id, osPriority priority)
+{
+    ds_osSetPriority_t ds;
+    osStatus result;
+
+    ds.thread_id = thread_id;
+    ds.priority = priority;
+    result = (osStatus)syscall(KERNEL_SYSCALL_SCHED_THREAD_SETPRIORITY, &ds);
+
+    return result;
+}
+
+osPriority osThreadGetPriority(osThreadId thread_id)
+{
+    osPriority result;
+
+    result = (osPriority)syscall(KERNEL_SYSCALL_SCHED_THREAD_GETPRIORITY, &thread_id);
+
+    return result;
+}
+
+/**
+  * @}
+  */
+
+/** @addtogroup Wait
+  * @{
+  */
+
+/* Generic Wait Functions ****************************************************/
+
+osStatus osDelay(uint32_t millisec)
+{
+    osStatus result;
+
+    result = (osStatus)syscall(KERNEL_SYSCALL_SCHED_DELAY, &millisec);
+
+    if (result != osErrorResource) {
+        /* Request context switch */
+        req_context_switch();
+    }
+
+    return result;
+}
+
+osEvent osWait(uint32_t millisec)
+{
+    osEvent * result;
+
+    result = (osEvent *)syscall(KERNEL_SYSCALL_SCHED_WAIT, &millisec);
+
+    if (result->status != osErrorResource) {
+        /* Request context switch */
+        req_context_switch();
+    }
+
+    /* Return a copy of the current state of the event structure */
+    return *result;
+}
+
+/**
+  * @}
+  */
+
+/** @addtogroup Signal
+  * @{
+  */
+
+/* Signal Management *********************************************************/
+
+int32_t osSignalSet(osThreadId thread_id, int32_t signal)
+{
+    ds_osSignal_t ds = { thread_id, signal };
+    int32_t result;
+
+    result = (int32_t)syscall(KERNEL_SYSCALL_SCHED_SIGNAL_SET, &ds);
+
+    return result;
+}
+
+int32_t osSignalClear(osThreadId thread_id, int32_t signal)
+{
+    ds_osSignal_t ds = { thread_id, signal };
+    int32_t result;
+
+    result = (int32_t)syscall(KERNEL_SYSCALL_SCHED_SIGNAL_CLEAR, &ds);
+
+    return result;
+}
+
+int32_t osSignalGetCurrent(void)
+{
+    int32_t result;
+
+    result = (int32_t)syscall(KERNEL_SYSCALL_SCHED_SIGNAL_GETCURR, NULL);
+
+    return result;
+}
+
+int32_t osSignalGet(osThreadId thread_id)
+{
+    int32_t result;
+
+    result = (int32_t)syscall(KERNEL_SYSCALL_SCHED_SIGNAL_GET, &thread_id);
+
+    return result;
+}
+
+osEvent osSignalWait(int32_t signals, uint32_t millisec)
+{
+    ds_osSignalWait_t ds = { signals, millisec };
+    osEvent * result;
+
+    result = (osEvent *)syscall(KERNEL_SYSCALL_SCHED_SIGNAL_WAIT, &ds);
+
+    if (result->status != osErrorResource) {
+        /* Request context switch */
+        req_context_switch();
+    }
+
+    /* Return a copy of the current state of the event structure */
+    return *result;
+}
+
+/**
+  * @}
+  */
+
+/** @addtogroup Mutex
+  * @{
+  */
+
+/* Mutex Management ***********************************************************
+ * NOTE: osMutexId/mutex_id is a direct pointer to a os_mutex_cb structure.
+ */
+
+/** TODO implement sleep strategy */
+
+osMutex osMutexCreate(osMutexDef_t *mutex_def)
+{
+    mutex_cb_t cb;
+
+    cb.thread_id = -1;
+    cb.lock = 0;
+    cb.strategy = mutex_def->strategy;
+
+    return cb;
+}
+
+osStatus osMutexWait(osMutex * mutex, uint32_t millisec)
+{
+    if (millisec != 0) {
+        /** TODO Only spinlock is supported at the moment; implement timeout */
+        return osErrorParameter;
+    }
+
+    while (syscall(KERNEL_SYSCALL_MUTEX_TEST_AND_SET, (void*)(&(mutex->lock)))) {
+        /** TODO Should we lower the priority until lock is acquired
+         *       osThreadGetPriority & osThreadSetPriority */
+        /* Reschedule while waiting for lock */
+        req_context_switch(); /* This should be done in user space */
+    }
+
+    mutex->thread_id = osThreadGetId();
+    return osOK;
+}
+
+osStatus osMutexRelease(osMutex * mutex)
+{
+    if (mutex->thread_id == osThreadGetId()) {
+        mutex->lock = 0;
+        return osOK;
+    }
+    return osErrorResource;
+}
+
+/**
+  * @}
+  */
+
+/** @addtogroup Dev_subsys
+  * @{
+  */
+
+/* Non-CMSIS *****************************************************************/
+
+#if configDEVSUBSYS == 1
+osEvent osDevWait(osDev_t dev, uint32_t millisec)
+{
+    ds_osDevWait_t ds = { dev, millisec };
+    osEvent * result;
+
+    result = (osEvent *)syscall(KERNEL_SYSCALL_SCHED_DEV_WAIT, &ds);
+
+    if (result->status != osErrorResource) {
+        /* Request context switch */
+        req_context_switch();
+    }
+
+    /* Return a copy of the current state of the event structure */
+    return *result;
+}
+#endif
+
+/**
+  * @}
+  */
+
 /**
   * @}
   */
