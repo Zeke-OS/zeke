438d0c9ab69295428fe6f01c8e1d9b6ca5b69761
Olli Vanhoja <olli.vanhoja@gmail.com>
2013-07-25 08:34:53 +0300

    Added __aeabi_memcpy4 & __aeabi_memcpy8

--- src/string/aeabi_memcpy.c
+++ src/string/aeabi_memcpy.c
@@ -3,17 +3,29 @@
  * @file    aeabi_memcpy.c
  * @author  Olli Vanhoja
  *
- * @brief   Wrapper for __aeabi_memcpy as clang expects it to exist even if
- *          there is no implementation for it in libc. __aeabi_memcpy and
- *          some variants of it are usually only implemented if cpu core has
+ * @brief   Wrappers for __aeabi_memcpy routines as clang expects it to exist
+ *          even if there is no implementation for it in libc. __aeabi_memcpy
+ *          and some variants of it are usually only implemented if cpu core has
  *          memcpy instructions.
+ *
+ *          Header file is not needed because these are only referenced by
+ *          clang and used by linker.
  *******************************************************************************
  */
 
 #include "string.h"
 
-/* Wrapper for aeabi_memcpy */
-void __aeabi_memcpy(void *dest, const void *src, ksize_t n)
+void __aeabi_memcpy(void *destination, const void *source, ksize_t num)
 {
-    memcpy (dest, src, n);
+    memcpy(destination, source, num);
+}
+
+void __aeabi_memcpy4(void *destination, const void *source, ksize_t num)
+{
+    memcpy(destination, source, num);
+}
+
+void __aeabi_memcpy8(void *destination, const void *source, ksize_t num)
+{
+        memcpy(destination, source, num);
 }
