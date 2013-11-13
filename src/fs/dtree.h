c51c783c2c0529dd5687bf4d148bdc8ad58d0074
Olli Vanhoja <olli.vanhoja@gmail.com>
2013-11-13 21:19:45 +0200

    Fixed copyrights

--- src/fs/dtree.h
+++ src/fs/dtree.h
@@ -6,7 +6,6 @@
  *          dtree is used for directory structure lookup caching.
  * @section LICENSE
  * Copyright (c) 2013 Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
- * Copyright (c) 2012, 2013, Ninjaware Oy, Olli Vanhoja <olli.vanhoja@ninjaware.fi>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
