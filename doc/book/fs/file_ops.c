2cabd76dbce0d883d3a1b0d840b1c9564d084e9c
Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
2014-01-31 22:27:27 +0200

    Updated documentation

--- doc/book/fs/file_ops.c
+++ doc/book/fs/file_ops.c
@@ -1,20 +1,30 @@
 /**
  * vnode operations struct.
  */
-typedef struct file_ops {
-    /* Normal file operations */
+typedef struct vnode_ops {
+    /* Normal file operations
+     * ---------------------- */
     int (*lock)(vnode_t * file);
     int (*release)(vnode_t * file);
-    int (*write)(vnode_t * file, int offset, const void * buf, int count);
-    int (*read)(vnode_t * file, int offset, void * buf, int count);
-    /* Directory file operations */
-    int (*create)(vnode_t * dir, const char * name, int name_len, vnode_t ** result);
-    int (*mknod)(vnode_t * dir, const char * name, int name_len, int mode, dev_t dev);
-    int (*lookup)(vnode_t * dir, const char * name, int name_len, vnode_t ** result);
-    int (*link)(vnode_t * oldvnode, vnode_t * dir, const char * name, int name_len);
-    int (*unlink)(vnode_t * dir, const char * name, int name_len);
-    int (*mkdir)(vnode_t * dir,  const char * name, int name_len);
-    int (*rmdir)(vnode_t * dir,  const char * name, int name_len);
+    size_t (*write)(vnode_t * file, const off_t * offset,
+            const void * buf, size_t count);
+    size_t (*read)(vnode_t * file, const off_t * offset,
+            void * buf, size_t count);
+    //int (*mmap)(vnode_t * file, !mem area!);
+    /* Directory file operations
+     * ------------------------- */
+    int (*create)(vnode_t * dir, const char * name, size_t name_len,
+            vnode_t ** result);
+    int (*mknod)(vnode_t * dir, const char * name, size_t name_len, int mode,
+            dev_t dev);
+    int (*lookup)(vnode_t * dir, const char * name, size_t name_len,
+            vnode_t ** result);
+    int (*link)(vnode_t * dir, vnode_t * vnode, const char * name,
+            size_t name_len);
+    int (*unlink)(vnode_t * dir, const char * name, size_t name_len);
+    int (*mkdir)(vnode_t * dir,  const char * name, size_t name_len);
+    int (*rmdir)(vnode_t * dir,  const char * name, size_t name_len);
+    int (*readdir)(vnode_t * dir, struct dirent * d);
     /* Operations specified for any file type */
     int (*stat)(vnode_t * vnode, struct stat * buf);
 } vnode_ops_t;
--- doc/book/fs/fs_superblock.c
+++ doc/book/fs/fs_superblock.c
@@ -8,7 +8,21 @@ typedef struct fs_superblock {
     vnode_t * root; /*!< Root of this fs mount. */
     char * mtpt_path; /*!< Mount point path */
 
+    /**
+     * Get the vnode struct linked to a vnode number.
+     * @param[in]  sb is the superblock.
+     * @param[in]  vnode_num is the vnode number.
+     * @param[out] vnode is a pointer to the vnode.
+     * @return Returns 0 if no error; Otherwise value other than zero.
+     */
     int (*get_vnode)(struct fs_superblock * sb, ino_t * vnode_num, vnode_t ** vnode);
+
+    /**
+     * Delete a vnode reference.
+     * Deletes a reference to a vnode and destroys the inode corresponding to the
+     * inode if there is no more links and references to it.
+     * @param[in] vnode is the vnode.
+     * @return Returns 0 if no error; Otherwise value other than zero.
+     */
     int (*delete_vnode)(vnode_t * vnode);
 } fs_superblock_t;
-
