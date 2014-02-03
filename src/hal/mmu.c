067b7c086c3ce1c9e8cf644d5d0ebe2c0dc23f59
Olli Vanhoja <olli.vanhoja@gmail.com>
2014-02-03 09:03:28 +0200

    Updated license information

--- src/hal/mmu.c
+++ src/hal/mmu.c
@@ -4,7 +4,7 @@
  * @author  Olli Vanhoja
  * @brief   MMU control.
  * @section LICENSE
- * Copyright (c) 2013 Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
+ * Copyright (c) 2013, 2014 Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
