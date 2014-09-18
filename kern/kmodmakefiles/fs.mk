34d24fddff0e1a9b80d07f0e03644d535d026488
Olli Vanhoja <olli.vanhoja@gmail.com>
2014-09-19 01:14:16 +0300

    Inpool as an optional part of the kernel

--- kern/kmodmakefiles/fs.mk
+++ kern/kmodmakefiles/fs.mk
@@ -1,5 +1,7 @@
 # File systems
 base-SRC-y += $(wildcard fs/*.c)
+# inode pools
+base-SRC-$(configFS_INPOOL) += $(wildcard fs/inpool/*.c)
 # VFS hash
 base-SRC-$(configVFS_HASH) += $(wildcard fs/vfs_hash/*.c)
 
