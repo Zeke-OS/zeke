e94335c6232b72f83ce026685400995e87425e5c
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-01-06 16:29:05 +0200

    Fix sigwait deadlock
    sigwait() was making SYSCALL_SIGNAL_SIGNAL syscalls instead of
    SYSCALL_SIGNAL_SIGWAIT and sometimes stayed in a deadlock due to
    that bug.

--- lib/libc/signal/sigwait.c
+++ lib/libc/signal/sigwait.c
@@ -4,7 +4,7 @@
  * @author  Olli Vanhoja
  * @brief   Wait for queued signals.
  * @section LICENSE
- * Copyright (c) 2014 Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
+ * Copyright (c) 2014, 2015 Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -50,5 +50,5 @@ int sigwait(const sigset_t * restrict set, int * restrict sig)
     if (!sig)
         args.sig = &tmp;
 
-    return syscall(SYSCALL_SIGNAL_SIGNAL, &args);
+    return syscall(SYSCALL_SIGNAL_SIGWAIT, &args);
 }
