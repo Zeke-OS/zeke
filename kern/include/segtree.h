e71ee49a89498b092fdd6127d1cb47863557ec05
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-02-01 02:02:13 +0200

    Doc comments

--- kern/include/segtree.h
+++ kern/include/segtree.h
@@ -36,6 +36,14 @@
 
 #include <stddef.h>
 
+/**
+ * Compares two given objects and returns either of them.
+ * This function shall implement min/max style comparison.
+ * The function should return a or b even if one or both of them are NULL.
+ * @param a first object.
+ * @param b second object.
+ * @return a or b.
+ */
 typedef void * (*segtcmp_t)(void * a, void * b);
 
 struct segt {
@@ -44,9 +52,35 @@ struct segt {
     void * arr[0];
 };
 
+/**
+ * Allocate and initialize a new segment tree.
+ * @param n is the size of the new tree.
+ * @param cmp is the compare function.
+ */
 struct segt * segt_init(size_t n, segtcmp_t cmp);
+
+/**
+ * Free a segment tree.
+ * @note Other resources shall be freed elsewhere, this only frees what
+ * segt_init() has allocated.
+ * @param s is a pointer to the segment tree.
+ */
 void segt_free(struct segt * s);
+
+/**
+ * Alter a segment tree.
+ * @param s is a pointer to the segment tree.
+ * @param k is the index.
+ * @param x is the new object.
+ */
 void segt_alt(struct segt * s, size_t k, void * x);
+
+/**
+ * Find a value between indices a and b.
+ * @param s is a pointer to the segment tree.
+ * @param a is the lower bound.
+ * @param b is the upper bound.
+ */
 void * segt_find(struct segt * s, size_t a, size_t b);
 
 #endif /* SEGTREE_H */
