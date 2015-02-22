9f2549d72631ab4bf3f063a3449ffd5f03e73a1e
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-02-22 20:39:51 +0200

    Cleanup

--- lib/libc/stdlib/system.c
+++ lib/libc/stdlib/system.c
@@ -1,33 +1,31 @@
-/* $Id$ */
-
-/* system( const char * )
-
-   This file is part of the Public Domain C Library (PDCLib).
-   Permission is granted to use, modify, and / or redistribute at will.
-*/
+/*
+ * system( const char * )
+ *
+ * This file is part of the Public Domain C Library (PDCLib).
+ * Permission is granted to use, modify, and / or redistribute at will.
+ */
 
 #include <stdlib.h>
 
-/* This is an example implementation of system() fit for use with POSIX kernels.
-*/
+/*
+ * This is an example implementation of system() fit for use with POSIX kernels.
+ */
 
 extern int fork( void );
 extern int execve( const char * filename, char * const argv[], char * const envp[] );
 extern int wait( int * status );
 
-int system( const char * string )
+int system(const char * string)
 {
     char const * const argv[] = { "sh", "-c", (char const * const)string, NULL };
-    if ( string != NULL )
-    {
+    if (string != NULL) {
         int pid = fork();
-        if ( pid == 0 )
-        {
-            execve( "/bin/sh", (char * * const)argv, NULL );
+        if (pid == 0) {
+            execve("/bin/sh", (char ** const)argv, NULL);
         }
-        else if ( pid > 0 )
+        else if (pid > 0)
         {
-            while( wait( NULL ) != pid );
+            while(wait( NULL ) != pid );
         }
     }
     return -1;
