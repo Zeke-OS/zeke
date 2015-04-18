e14dfc00a9f48b272d3f5b034c7e07a8078cc465
Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
2015-04-18 15:43:35 +0300

    Add exec to qemu scripts

--- tools/qemuscripts/runscript.sh
+++ tools/qemuscripts/runscript.sh
@@ -8,7 +8,7 @@ fi
 # QEMU makes decissions based on the filename given and we are more compatible
 # with the linux behavior than the non-linux way of loading a kernel image.
 ln -sf kernel.elf vmlinux-kernel.elf
-./tools/exec_qemu "$1" "$2" qemu-system-arm -kernel vmlinux-kernel.elf \
+exec tools/exec_qemu "$1" "$2" qemu-system-arm -kernel vmlinux-kernel.elf \
     -cpu arm1176 -m 256 -M raspi -nographic -serial stdio \
     -d unimp,guest_errors -s -sd zeke-rootfs.img \
     -append 'console=ttyS0,115200 root=/dev/emmc0p1 rootfstype=fatfs'
