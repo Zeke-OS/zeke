94503e65831cd38b4811748aff647e99b277bae9
Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
2015-04-18 16:09:33 +0300

    Don't wait for gdb

--- tools/qemuscripts/runscript.sh
+++ tools/qemuscripts/runscript.sh
@@ -10,5 +10,6 @@ fi
 ln -sf kernel.elf vmlinux-kernel.elf
 exec tools/exec_qemu "$1" "$2" qemu-system-arm -kernel vmlinux-kernel.elf \
     -cpu arm1176 -m 256 -M raspi -nographic -serial stdio \
-    -d unimp,guest_errors -s -sd zeke-rootfs.img \
+    -monitor telnet::4444,server,nowait -d unimp,guest_errors -s \
+    -sd zeke-rootfs.img \
     -append 'console=ttyS0,115200 root=/dev/emmc0p1 rootfstype=fatfs'
