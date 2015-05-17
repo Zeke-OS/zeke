69eee028430af676a3e3b4d9622ab31f9d294906
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-05-17 13:04:31 +0300

    More pipe() tests

--- opt/test/unistd/test_pipe.c
+++ opt/test/unistd/test_pipe.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include "punit.h"
 
@@ -29,7 +31,6 @@ static char * test_simple(void)
     memset(str, '\0', sizeof(str));
 
     pu_assert_equal("pipe creation ok", pipe(fd), 0);
-
     pu_assert("sane fd[0]", fd[0] > 0);
     pu_assert("sane fd[1]", fd[1] > 0);
 
@@ -48,16 +49,69 @@ static char * test_eof(void)
     char str[] = "testing";
 
     pu_assert_equal("pipe creation ok", pipe(fd), 0);
-
     pu_assert("sane fd[0]", fd[0] > 0);
     pu_assert("sane fd[1]", fd[1] > 0);
 
     write(fd[1], str, sizeof(str));
     read(fd[0], str, sizeof(str));
     close(fd[1]);
+    fd[1] = 0;
 
     pu_assert("EOF returned", read(fd[0], str, sizeof(str)) == EOF);
 
+    return NULL;
+}
+
+static char * test_eof_remaining(void)
+{
+    char str[] = "testing";
+
+    pu_assert_equal("pipe creation ok", pipe(fd), 0);
+    pu_assert("sane fd[0]", fd[0] > 0);
+    pu_assert("sane fd[1]", fd[1] > 0);
+
+    write(fd[1], str, sizeof(str));
+    close(fd[1]);
+    fd[1] = 0;
+    pu_assert("read() ok", read(fd[0], str, sizeof(str)) == sizeof(str));
+
+    return NULL;
+}
+
+static char * test_pipe_after_fork(void)
+{
+#define STR "testing"
+    const char tstr[] = STR;
+    pid_t pid;
+
+    pu_assert_equal("pipe creation ok", pipe(fd), 0);
+    pu_assert("sane fd[0]", fd[0] > 0);
+    pu_assert("sane fd[1]", fd[1] > 0);
+
+    pid = fork();
+    pu_assert("PID OK\n", pid != -1);
+    if (pid == 0) {
+        close(fd[0]);
+        write(fd[1], tstr, sizeof(tstr));
+
+        _exit(0);
+    } else {
+        ssize_t n, i = 0;
+        char str[sizeof(STR)] = "";
+
+        close(fd[1]);
+        while ((n = read(fd[0], str + i, 1))) {
+            pu_assert("Not end of file yet", n != EOF);
+            pu_assert("Only one char was read", n == 1);
+            pu_assert_equal("Proper char was received", *(str + i), tstr[i]);
+            if (++i == sizeof(str))
+                break;
+        }
+    }
+
+    wait(NULL);
+
+#undef STR
     return NULL;
 }
 
@@ -65,6 +119,8 @@ static void all_tests(void)
 {
     pu_def_test(test_simple, PU_RUN);
     pu_def_test(test_eof, PU_RUN);
+    pu_def_test(test_eof_remaining, PU_RUN);
+    pu_def_test(test_pipe_after_fork, PU_RUN);
 }
 
 int main(int argc, char **argv)
