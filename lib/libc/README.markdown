7b0f89f02ea4c5165ce0686264d5b3f5a49a7b26
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-05-28 07:54:43 +0300

    Cleanup libc
    Move setenv() and getenv() to stdlib
    Remove duplicated getenv()

--- lib/libc/README.markdown
+++ lib/libc/README.markdown
@@ -4,14 +4,3 @@ Zeke libc
 Zeke libc is a partially BSD compatible C standard library tailored for Zeke.
 
 Header files for Zeke libc are located under ../../include in this source tree.
-
-Status
-------
-
-Currently there is not much files under this directory and no makefiles and
-everything is mostly useless here but some day there will be a full-blown
-libc.
-
-At the moment we are compiling and linking c files in this directory directly
-with the kernel for testing purposes and everything in sub dirs is ignored
-for now.
--- lib/libc/stdlib/getenv.c
+++ lib/libc/stdlib/getenv.c
@@ -1,24 +1,57 @@
 /*
- * getenv(const char *)
- *
- * This file is part of the Public Domain C Library (PDCLib).
- * Permission is granted to use, modify, and / or redistribute at will.
+ * Copyright (c) 2014 Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
+ * Copyright (c) 1987 Regents of the University of California.
+ * This file may be freely redistributed provided that this
+ * notice remains attached.
  */
-
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
-extern char ** environ;
+extern char * _findenv(const char * name, size_t * offset);
 
+/*
+ * getenv(name) --
+ *  Returns ptr to value associated with name, if any, else NULL.
+ */
 char * getenv(const char * name)
 {
-    size_t len = strlen(name);
-    size_t index = 0;
-    while (environ[index] != NULL) {
-        if (strncmp(environ[index], name, len) == 0) {
-            return environ[index] + len + 1;
-        }
-        index++;
+    size_t offset;
+
+    return _findenv(name, &offset);
+}
+
+/*
+ * _findenv(name,offset) --
+ *  Returns pointer to value associated with name, if any, else NULL.
+ *  Sets offset to be the offset of the name/value combination in the
+ *  environmental array, for use by setenv(3) and unsetenv(3).
+ *  Explicitly removes '=' in argument name.
+ *
+ *  This routine *should* be a static; don't use it.
+ */
+char * _findenv(const char * name, size_t * offset)
+{
+    size_t len;
+    const char * e;
+    char ** p;
+    char * c;
+
+    len = 0;
+    for (e = name; *e && *e != '='; ++e) {
+                ++len;
     }
+
+    for (p = environ; *p; ++p) {
+        if (!strncmp(*p, name, len)) {
+            if (*(c = *p + len) == '=') {
+                *offset = p - environ;
+                return ++c;
+            }
+        }
+    }
+
     return NULL;
 }
