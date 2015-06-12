1a16404560d9a6ee1755d6b6a7482a857538bab2
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-06-13 02:53:57 +0300

    Remove _USE_STRFUNC functions

--- kern/fs/fatfs/src/ffconf.h
+++ kern/fs/fatfs/src/ffconf.h
@@ -37,11 +37,6 @@
  *  3: f_lseek() function is removed in addition to 2.
  */
 
-
-#define _USE_STRFUNC    0   /* 0:Disable or 1-2:Enable */
-/* To enable string functions, set _USE_STRFUNC to 1 or 2. */
-
-
 #define _USE_MKFS       0   /* 0:Disable or 1:Enable */
 /* To enable f_mkfs() function, set _USE_MKFS to 1 and set _FS_READONLY to 0 */
 
