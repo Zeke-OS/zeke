d8c091ea98a3cf149fe3b43de96ba187dda8bf55
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-07-25 15:50:23 +0300

    tish: Support PS1 env

--- bin/src/sh/tish.c
+++ bin/src/sh/tish.c
@@ -34,6 +34,7 @@
 
 #include <autoconf.h> /* for KERNEL_VERSION */
 #include <fcntl.h>
+#include <libgen.h>
 #include <limits.h>
 #include <linenoise.h>
 #include <stdio.h>
@@ -56,6 +57,7 @@ char banner[] = "\
 ";
 static const char msg[] = "Zeke " KERNEL_VERSION;
 static char histfilepath[256];
+static char * argv0;
 
 void init_hist(void)
 {
@@ -88,10 +90,96 @@ void init_hist(void)
     linenoiseHistoryLoad(histfilepath);
 }
 
+
+static char * get_prompt(void)
+{
+    char * ps1;
+    size_t i = 0;
+    static char prompt[40];
+    char buf[40];
+
+    ps1 = getenv("PS1");
+    if (!ps1) {
+        ps1 = "# ";
+        return ps1;
+    }
+
+    while (*ps1 != '\0') {
+        if (i >= sizeof(prompt) - 1)
+            break;
+
+        if (*ps1 != '\\') {
+            prompt[i++] = *ps1++;
+            continue;
+        }
+        if (*++ps1 == '\0')
+            break;
+        switch (*ps1) {
+        case 'a': /* Bell */
+            prompt[i++] = '\a';
+            break;
+        case 'd': /* TODO Date eg. "Tue May 26" */
+            break;
+        case 'e':
+            prompt[i++] = '\033';
+            break;
+        case 'h':
+        case 'H':
+            if (!gethostname(buf, sizeof(buf))) {
+                i += strlcpy(prompt + i, buf, sizeof(prompt) - i);
+            }
+            break;
+        case 'n':
+            prompt[i++] = '\n';
+            break;
+        case 'r':
+            prompt[i++] = '\r';
+            break;
+        case 's':
+            i += strlcpy(prompt + i, argv0, sizeof(prompt) - i);
+            break;
+        /* TODO t, T, and @ */
+        case 't':
+            break;
+        case 'T':
+            break;
+        case '@':
+            break;
+        case 'u': /* TODO username of the current user */
+            break;
+        case 'w':
+        case 'W': {
+                char * pwd;
+                char * pwd2;
+
+                pwd = getcwd(buf, sizeof(buf));
+                if (!pwd)
+                    break;
+                if (*ps1 == 'W' && (pwd2 = basename(pwd)))
+                    pwd = pwd2;
+                if (pwd)
+                    i += strlcpy(prompt + i, pwd, sizeof(prompt) - i);
+                break;
+            }
+        case '$':
+            prompt[i++] = (getuid() == 0) ? '#' : '$';
+            break;
+        case '\\':
+            prompt[i++] = '\\';
+            break;
+        }
+        ps1++;
+    }
+    prompt[sizeof(prompt) - 1] = '\0';
+
+    return prompt;
+}
+
 int main(int argc, char * argv[], char * envp[])
 {
-    const char prompt[] = "# ";
     char * line;
+
+    argv0 = argv[0];
 
     /* TODO get opts */
     argv += 1;
@@ -124,7 +212,7 @@ int main(int argc, char * argv[], char * envp[])
         init_hist();
 
         fflush(NULL);
-        while ((line = linenoise(prompt)) != NULL) {
+        while ((line = linenoise(get_prompt())) != NULL) {
             linenoiseHistoryAdd(line);
             linenoiseHistorySave(histfilepath);
 
