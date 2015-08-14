bcae46ea1494b162d1ed0f412a9111066a5f569f
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-08-14 09:09:20 +0300

    Cleanup div.c

---
+++ opt/test/libc/test_div.c
@@ -0,0 +1,42 @@
+#include <ctype.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/_PDCLIB_int.h>
+#include "punit.h"
+
+static void setup(void)
+{
+}
+
+static void teardown(void)
+{
+}
+
+char * test_div(void)
+{
+    div_t result;
+
+    result = div(5, 2);
+    pu_assert("", result.quot == 2 && result.rem == 1);
+
+    result = div(-5, 2);
+    pu_assert("", result.quot == -2 && result.rem == -1);
+
+    result = div(5, -2);
+    pu_assert("", result.quot == -2 && result.rem == 1);
+    pu_assert_equal"", (sizeof(result.quot), sizeof(int));
+    pu_assert_equal("", sizeof(result.rem), sizeof(int));
+
+    return TEST_RESULTS;
+}
+
+static void all_tests(void)
+{
+    pu_def_test(test_div, PU_RUN);
+}
+
+int main(int argc, char ** argv)
+{
+    return pu_run_tests(&all_tests);
+}
