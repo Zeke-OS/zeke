c68b35a24be0d168d6b7063e6d5285c7f94de5b1
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-08-14 09:23:53 +0300

    Cleanup ldiv() and lldiv()

---
+++ opt/test/libc/test_ldiv.c
@@ -0,0 +1,42 @@
+#include <ctype.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/_PDCLIB_int.h>
+#include "punit.h"
+
+static void setup(void)
+{
+}
+
+static void teardown(void)
+{
+}
+
+char * test_ldiv(void)
+{
+    ldiv_t result;
+
+    result = ldiv(5, 2);
+    pu_assert("", result.quot == 2 && result.rem == 1);
+
+    result = ldiv(-5, 2);
+    pu_assert("", result.quot == -2 && result.rem == -1);
+
+    result = ldiv(5, -2);
+    pu_assert("", result.quot == -2 && result.rem == 1);
+    pu_assert_equal("", sizeof(result.quot), sizeof(long));
+    pu_assert_equal("", sizeof(result.rem), sizeof(long));
+
+    return NULL;
+}
+
+static void all_tests(void)
+{
+    pu_def_test(test_ldiv, PU_RUN);
+}
+
+int main(int argc, char ** argv)
+{
+    return pu_run_tests(&all_tests);
+}
---
+++ opt/test/libc/test_lldiv.c
@@ -0,0 +1,42 @@
+#include <ctype.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/_PDCLIB_int.h>
+#include "punit.h"
+
+static void setup(void)
+{
+}
+
+static void teardown(void)
+{
+}
+
+char * test_lldiv(void)
+{
+    lldiv_t result;
+
+    result = lldiv(5, 2);
+    pu_assert("", result.quot == 2 && result.rem == 1);
+
+    result = lldiv(-5, 2);
+    pu_assert("", result.quot == -2 && result.rem == -1);
+
+    result = lldiv(5, -2);
+    pu_assert("", result.quot == -2 && result.rem == 1);
+    pu_assert_equal("", sizeof(result.quot), sizeof(long long));
+    pu_assert_equal("", sizeof(result.rem), sizeof(long long));
+
+    return NULL;
+}
+
+static void all_tests(void)
+{
+    pu_def_test(test_lldiv, PU_RUN);
+}
+
+int main(int argc, char ** argv)
+{
+    return pu_run_tests(&all_tests);
+}
