6259cd4bea8ee41bba0b7a146ae9c9ff6e9af8c9
Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
2015-08-29 23:52:10 +0300

    Support for all list types in queue.h

--- tools/gdb/zeke_queue.py
+++ tools/gdb/zeke_queue.py
@@ -1,36 +1,110 @@
 import gdb
 
-class TAILQPrint(gdb.Command):
-    "Print a TAILQ\nArgs: <head> <field>"
+class QueuePrint(gdb.Command):
+    "Print list defined in queue.h"
 
     def __init__(self):
-        super(TAILQPrint, self).__init__("print-tailq", 
-                gdb.COMMAND_DATA, gdb.COMPLETE_SYMBOL)
+        super(QueuePrint, self).__init__("print-queue",
+                gdb.COMMAND_DATA, gdb.COMPLETE_NONE, True)
+
+class SLISTPrint(gdb.Command):
+    "Print SLIST\nArgs: <head> <field>"
+
+    def __init__(self):
+        super(SLISTPrint, self).__init__("print-queue slist",
+                gdb.COMMAND_DATA, gdb.COMPLETE_EXPRESSION)
 
     def invoke(self, arg, from_tty):
-        args = gdb.string_to_argv(arg)
-
-        if len(args) < 2:
-            gdb.write('nr args < 2\n')
-            return
-        
-        try:
-            head = gdb.parse_and_eval(args[0])
-        except:
-            gdb.write('No symbol "%s" in current context.\n' % str(args[0]))
+        args = getArgs(arg)
+        if args is None:
             return
 
-        print getTAILQ(head, args[1])
+        print getSLIST(args[0], args[1])
+
+class LISTPrint(gdb.Command):
+    "Print LIST\nArgs: <head> <field>"
+
+    def __init__(self):
+        super(LISTPrint, self).__init__("print-queue list",
+                gdb.COMMAND_DATA, gdb.COMPLETE_EXPRESSION)
+
+    def invoke(self, arg, from_tty):
+        args = getArgs(arg)
+        if args is None:
+            return
+
+        print getLIST(args[0], args[1])
+
+class STAILQPrint(gdb.Command):
+    "Print STAILQ\nArgs: <head> <field>"
+
+    def __init__(self):
+        super(STAILQPrint, self).__init__("print-queue stailq",
+                gdb.COMMAND_DATA, gdb.COMPLETE_EXPRESSION)
+
+    def invoke(self, arg, from_tty):
+        args = getArgs(arg)
+        if args is None:
+            return
+
+        print getSTAILQ(args[0], args[1])
+
+class TAILQPrint(gdb.Command):
+    "Print TAILQ\nArgs: <head> <field>"
+
+    def __init__(self):
+        super(TAILQPrint, self).__init__("print-queue tailq",
+                gdb.COMMAND_DATA, gdb.COMPLETE_EXPRESSION)
+
+    def invoke(self, arg, from_tty):
+        args = getArgs(arg)
+        if args is None:
+            return
+
+        print getTAILQ(args[0], args[1])
+
+def getArgs(arg):
+    args = gdb.string_to_argv(arg)
+
+    if len(args) < 2:
+        gdb.write('nr args < 2\n')
+        return None
+
+    try:
+        head = gdb.parse_and_eval(args[0])
+    except:
+        gdb.write('No symbol "%s" in current context.\n' % str(args[0]))
+        return None
+
+    return [head, args[1]]
+
+def getSLIST(head, field):
+    return _getQUEUE(head, field, 'slh_first', 'sle_next')
+
+def getLIST(head, field):
+    return _getQUEUE(head, field, 'lh_first', 'le_next')
+
+def getSTAILQ(head, field):
+    return _getQUEUE(head, field, 'stqh_first', 'stqe_next')
 
 def getTAILQ(head, field):
-    node = head['tqh_first']
+    return _getQUEUE(head, field, 'tqh_first', 'tqe_next')
+
+def _getQUEUE(head, field, head_field, next_field):
+    node = head[head_field]
 
     q = []
     while node != 0:
         q.append(str(node.dereference()))
-        node = node[field]['tqe_next']
+        ep_str = '((' + str(node.type) + ')' + str(node.dereference().address) + ')->' + field
+        ep = gdb.parse_and_eval(ep_str)
+        node = ep[next_field]
 
     return q
 
 
+QueuePrint()
+SLISTPrint()
+LISTPrint()
+STAILQPrint()
 TAILQPrint()
