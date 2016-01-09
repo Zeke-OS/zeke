94b3530fdf47cd499d1d83f71a0866ad51f85fe7
Olli Vanhoja <olli.vanhoja@gmail.com>
2016-01-09 04:16:11 +0200

    Fix kactype.h coding style

--- kern/include/kactype.h
+++ kern/include/kactype.h
@@ -3,7 +3,7 @@
  *
  * This is the ctype from 2.11BSD. This old ctype header and ctype array is
  * included in the kernel to make fast verifications against ASCII C-strings.
- * 2015 Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
+ * 2015, 2016 Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
  */
 
 /**
@@ -31,21 +31,21 @@
 
 extern char _kactype_[];
 
-#define ka_isalpha(c)   ((_kactype_+1)[(int)(c)]&(_U|_L))
-#define ka_isupper(c)   ((_kactype_+1)[(int)(c)]&_U)
-#define ka_islower(c)   ((_kactype_+1)[(int)(c)]&_L)
-#define ka_isdigit(c)   ((_kactype_+1)[(int)(c)]&_N)
-#define ka_isxdigit(c)  ((_kactype_+1)[(int)(c)]&(_N|_X))
-#define ka_isspace(c)   ((_kactype_+1)[(int)(c)]&_S)
-#define ka_ispunct(c)   ((_kactype_+1)[(int)(c)]&_P)
-#define ka_isalnum(c)   ((_kactype_+1)[(int)(c)]&(_U|_L|_N))
-#define ka_isprint(c)   ((_kactype_+1)[(int)(c)]&(_P|_U|_L|_N|_B))
-#define ka_isgraph(c)   ((_kactype_+1)[(int)(c)]&(_P|_U|_L|_N))
-#define ka_iscntrl(c)   ((_kactype_+1)[(int)(c)]&_C)
-#define ka_isascii(c)   ((unsigned)(c)<=0177)
-#define ka_toupper(c)   ((c)-'a'+'A')
-#define ka_tolower(c)   ((c)-'A'+'a')
-#define ka_toascii(c)   ((c)&0177)
+#define ka_isalpha(c)   ((_kactype_ + 1)[(int)(c)] & (_U | _L))
+#define ka_isupper(c)   ((_kactype_ + 1)[(int)(c)] & _U)
+#define ka_islower(c)   ((_kactype_ + 1)[(int)(c)] & _L)
+#define ka_isdigit(c)   ((_kactype_ + 1)[(int)(c)] & _N)
+#define ka_isxdigit(c)  ((_kactype_ + 1)[(int)(c)] & (_N | _X))
+#define ka_isspace(c)   ((_kactype_ + 1)[(int)(c)] & _S)
+#define ka_ispunct(c)   ((_kactype_ + 1)[(int)(c)] & _P)
+#define ka_isalnum(c)   ((_kactype_ + 1)[(int)(c)] & (_U | _L | _N))
+#define ka_isprint(c)   ((_kactype_ + 1)[(int)(c)] & (_P | _U | _L | _N | _B))
+#define ka_isgraph(c)   ((_kactype_ + 1)[(int)(c)] & (_P | _U | _L | _N))
+#define ka_iscntrl(c)   ((_kactype_ + 1)[(int)(c)] & _C)
+#define ka_isascii(c)   ((unsigned)(c) <= 0177)
+#define ka_toupper(c)   ((c) - 'a' + 'A')
+#define ka_tolower(c)   ((c) - 'A' + 'a')
+#define ka_toascii(c)   ((c) & 0177)
 
 #endif /* KACTYPE_H */
 
