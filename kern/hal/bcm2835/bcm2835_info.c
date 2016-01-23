7d7711c43ca4ab068a0383e9d19c76561495ca40
Olli Vanhoja <olli.vanhoja@gmail.com>
2016-01-23 05:25:56 +0200

    bcm2835_info: try to parse rpi model name

--- kern/hal/bcm2835/bcm2835_info.c
+++ kern/hal/bcm2835/bcm2835_info.c
@@ -38,6 +38,22 @@
 #include <libkern.h>
 #include "bcm2835_prop.h"
 
+static const char * const rpi_rev_name[] = {
+    [0x2] = "B1",
+    [0x3] = "B1+",
+    [0x4] = "B2",
+    [0x5] = "B2",
+    [0x6] = "B2",
+    [0x7] = "A",
+    [0x8] = "A",
+    [0x9] = "A",
+    [0xD] = "B2",
+    [0xE] = "B2",
+    [0xF] = "B2",
+
+};
+#define RPI_REV_NAME_MAX 4
+
 static int get_info_prop(uint32_t * value, size_t value_size, uint32_t tag)
 {
     size_t wc = value_size / 4;
@@ -64,6 +80,32 @@ static int get_info_prop(uint32_t * value, size_t value_size, uint32_t tag)
     return 0;
 }
 
+static void get_hw_model(void)
+{
+    uint32_t model = 0, rev_id = 0;
+    char rev_str[25] = "";
+    char hw_model[40];
+    int ctl_name[] = { CTL_HW, HW_MODEL };
+
+    get_info_prop(&model, sizeof(model),
+                  BCM2835_PROP_TAG_GET_GET_BOARD_MODEL);
+
+    if (!get_info_prop(&rev_id, sizeof(rev_id),
+                       BCM2835_PROP_TAG_GET_BOARD_REVISION)) {
+        if (rev_id < num_elem(rpi_rev_name) &&
+            strlenn(rpi_rev_name[rev_id], RPI_REV_NAME_MAX) > 0) {
+            ksprintf(rev_str, sizeof(rev_str), " Raspberry Pi model %s",
+                     rpi_rev_name[rev_id]);
+        }
+    }
+
+    ksprintf(hw_model, sizeof(hw_model), "BCM2835 board model %u%s",
+             (unsigned)model, rev_str);
+    KERROR(KERROR_DEBUG, "%s\n", hw_model);
+    kernel_sysctl_write(ctl_name, num_elem(ctl_name),
+                        hw_model, strlenn(hw_model, sizeof(hw_model)));
+}
+
 int __kinit__ bcm2835_info_init(void)
 {
     SUBSYS_DEP(bcm2835_prop_init);
@@ -71,24 +113,13 @@ int __kinit__ bcm2835_info_init(void)
 
     uint32_t value[2];
 
-    /*
-     * HW_MODEL
-     */
-    if (!get_info_prop(value, 4, BCM2835_PROP_TAG_GET_GET_BOARD_MODEL)) {
-        char hw_model[20];
-        int ctl_name[] = { CTL_HW, HW_MODEL };
-
-        ksprintf(hw_model, sizeof(hw_model), "BCM2835 model %d", value[0]);
-        kernel_sysctl_write(ctl_name, num_elem(ctl_name),
-                            hw_model, sizeof(hw_model));
-    } else {
-        KERROR(KERROR_WARN, "%s: Failed to get board model\n", __func__);
-    }
+    get_hw_model();
 
     /*
      * HW_PHYSMEM_START & HW_PHYSMEM
      */
-    if (!get_info_prop(value, 8, BCM2835_PROP_TAG_GET_ARM_MEMORY)) {
+    if (!get_info_prop(value, sizeof(value),
+                       BCM2835_PROP_TAG_GET_ARM_MEMORY)) {
         int ctl_start[] = { CTL_HW, HW_PHYSMEM_START };
         int ctl_size[] = { CTL_HW, HW_PHYSMEM };
 
