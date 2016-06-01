0af06c63100bd9ea751376e499cf9db8669d9136
Olli Vanhoja <olli.vanhoja@gmail.com>
2016-06-02 01:07:17 +0300

    Compile dyndebug by default

--- kern/kmodmakefiles/base.mk
+++ kern/kmodmakefiles/base.mk
@@ -4,6 +4,7 @@ base-SRC-y += $(wildcard sched/*.c)
 
 # Kernel logging
 base-SRC-$(configKLOGGER) += kerror/kerror.c
+base-SRC-$(configKLOGGER) += kerror/dyndebug.c
 base-SRC-$(configKLOGGER) += kerror/kerror_buf.c
 base-SRC-$(configKERROR_UART) += kerror/kerror_uart.c
 base-SRC-$(configKERROR_FB) += kerror/kerror_fb.c
