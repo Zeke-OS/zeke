9fd74f6b609fd6e4166fd631d878ac3afc305a5d
Olli Vanhoja <olli.vanhoja@gmail.com>
2019-08-17 01:35:10 +0300

    Remove sysctls that won't be implemented

--- include/sys/sysctl.h
+++ include/sys/sysctl.h
@@ -523,13 +523,8 @@ SYSCTL_ALLOWED_TYPES(UINT64, uint64_t *a; unsigned long long *b; );
  * KERN_PROC subtypes
  */
 #define KERN_PROC_PID           1   /*!< Get proc data by process id */
-#define KERN_PROC_PGRP          2   /*!< Get PIDs by process group id */
-#define KERN_PROC_SESSION       3   /*!< Get PIDs session of pid */
-#define KERN_PROC_TTY           4   /*!< Get PIDs controlling tty */
-#define KERN_PROC_UID           5   /*!< Get PIDs effective uid */
-#define KERN_PROC_RUID          6   /*!< Get PIDs real uid */
-#define KERN_PROC_RGID          7   /*!< Get PIDs by real group id */
-#define KERN_PROC_GID           8   /*!< Get PIds by effective group id */
+#define KERN_PROC_PGRP          2   /*!< Get process group info */
+#define KERN_PROC_SESSION       3   /*!< Get session info */
 
 /*
  * KERN_PROC_PID subtypes
