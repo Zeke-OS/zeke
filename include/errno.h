3bbf8a4881e5a010ae546ad0c903aecf0af4054f
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-07-18 00:18:12 +0200

    Cleanup errno.h

--- include/errno.h
+++ include/errno.h
@@ -5,6 +5,7 @@
  * @brief   System error numbers.
  *          IEEE Std 1003.1, 2013
  * @section LICENSE
+ * Copyright (c) 2020 Olli Vanhoja <olli.vanhoja@alumni.helsinki.fi>
  * Copyright (c) 2013, 2015 - 2016 Olli Vanhoja <olli.vanhoja@cs.helsinki.fi>
  * All rights reserved.
  *
@@ -46,86 +47,402 @@
 #ifndef ERRNO_H
 #define ERRNO_H
 
-#define E2BIG           1   /*!< Argument list too long. */
-#define EACCES          2   /*!< Permission denied. */
-#define EADDRINUSE      3   /*!< Address in use. */
-#define EADDRNOTAVAIL   4   /*!< Address not available. */
-#define EAFNOSUPPORT    5   /*!< Address family not supported. */
-#define EAGAIN          6   /*!< Resource unavailable, try again. */
-#define EALREADY        7   /*!< Connection already in progress. */
-#define EBADF           8   /*!< Bad file descriptor. */
-#define EBADMSG         9   /*!< Bad message. */
-#define EBUSY           10  /*!< Device or resource busy. */
-#define ECANCELED       11  /*!< Operation canceled. */
-#define ECHILD          12  /*!< No child processes. */
-#define ECONNABORTED    13  /*!< Connection aborted. */
-#define ECONNREFUSED    14  /*!< Connection refused. */
-#define ECONNRESET      15  /*!< Connection reset. */
-#define EDEADLK         16  /*!< Resource deadlock would occur. */
-#define EDESTADDRREQ    17  /*!< Destination address required. */
-#define EDOM            18  /*!< Mathematics argument out of domain of
-                             *   function. */
-#define EDQUOT          19  /*!< Reserved. */
-#define EEXIST          20  /*!< File exists. */
-#define EFAULT          21  /*!< Bad address. */
-#define EFBIG           22  /*!< File too large. */
-#define EHOSTUNREACH    23  /*!< Host is unreachable. */
-#define EIDRM           24  /*!< Identifier removed. */
-#define EILSEQ          25  /*!< Illegal byte sequence. */
-#define EINPROGRESS     26  /*!< Operation in progress. */
-#define EINTR           27  /*!< Interrupted function. */
-#define EINVAL          28  /*!< Invalid argument. */
-#define EIO             29  /*!< I/O error. */
-#define EISCONN         30  /*!< Socket is connected. */
-#define EISDIR          31  /*!< Is a directory. */
-#define ELOOP           32  /*!< Too many levels of symbolic links. */
-#define EMFILE          33  /*!< File descriptor value too large. */
-#define EMLINK          34  /*!< Too many links. */
-#define EMSGSIZE        35  /*!< Message too large. */
-#define EMULTIHOP       36  /*!< Reserved. */
-#define ENAMETOOLONG    37  /*!< Filename too long. */
-#define ENETDOWN        38  /*!< Network is down. */
-#define ENETRESET       39  /*!< Connection aborted by network. */
-#define ENETUNREACH     40  /*!< Network unreachable. */
-#define ENFILE          41  /*!< Too many files open in system. */
-#define ENOBUFS         42  /*!< No buffer space available. */
-#define ENODEV          44  /*!< No such device. */
-#define ENOENT          45  /*!< No such file or directory. */
-#define ENOEXEC         46  /*!< Executable file format error. */
-#define ENOLCK          47  /*!< No locks available. */
-#define ENOLINK         48  /*!< Link has been severed. */
-#define ENOMEM          49  /*!< Not enough space. */
-#define ENOMSG          50  /*!< No message of the desired type. */
-#define ENOPROTOOPT     51  /*!< Protocol not available. */
-#define ENOSPC          52  /*!< No space left on device. */
-#define ENOSYS          55  /*!< Function not supported. */
-#define ENOTCONN        56  /*!< The socket is not connected. */
-#define ENOTDIR         57  /*!< Not a directory or a symbolic link to a
-                             *   directory. */
-#define ENOTEMPTY       58  /*!< Directory not empty. */
-#define ENOTRECOVERABLE 59  /*!< State not recoverable. */
-#define ENOTSOCK        60  /*!< Not a socket. */
-#define ENOTSUP         61  /*!< Not supported. */
-#define ENOTTY          62  /*!< Inappropriate I/O control operation. */
-#define ENXIO           63  /*!< No such device or address. */
-#define EOPNOTSUPP      ENOTSUP /*!< Operation not supported on socket */
-#define EOVERFLOW       64  /*!< Value too large to be stored in data type. */
-#define EOWNERDEAD      66  /*!< Previous owner died. */
-#define EPERM           67  /*!< Operation not permitted. */
-#define EPIPE           68  /*!< Broken pipe. */
-#define EPROTO          69  /*!< Protocol error. */
-#define EPROTONOSUPPORT 70  /*!< Protocol not supported. */
-#define EPROTOTYPE      71  /*!< Protocol wrong type for socket. */
-#define ERANGE          72  /*!< Result too large. */
-#define EROFS           73  /*!< Read-only file system. */
-#define ESPIPE          74  /*!< Invalid seek. */
-#define ESRCH           75  /*!< No such process. */
-#define ESTALE          76  /*!< Stale file handle. */
-#define ETIMEDOUT       78  /*!< Connection timed out. */
-#define ETXTBSY         79  /*!< Text file busy. */
-#define EWOULDBLOCK     EAGAIN /*!< Operation would block */
-#define EXDEV           80  /*!< Cross-device link or Improper link. */
-#define ENOTBLK         81  /*!< Block device required */
+/**
+ * Argument list too long.
+ * @since POSIX.1-2017
+ */
+#define E2BIG           1
+/**
+ * Permission denied.
+ * @since POSIX.1-2017
+ */
+#define EACCES          2
+/**
+ * Address in use.
+ * @since POSIX.1-2017
+ */
+#define EADDRINUSE      3
+/**
+ * Address not available.
+ * @since POSIX.1-2017
+ */
+#define EADDRNOTAVAIL   4
+/**
+ * Address family not supported.
+ * @since POSIX.1-2017
+ */
+#define EAFNOSUPPORT    5
+/**
+ * Resource unavailable, try again.
+ * @since POSIX.1-2017
+ */
+#define EAGAIN          6
+/**
+ * Connection already in progress.
+ * @since POSIX.1-2017
+ */
+#define EALREADY        7
+/**
+ * Bad file descriptor.
+ * @since POSIX.1-2017
+ */
+#define EBADF           8
+/**
+ * Bad message.
+ * @since POSIX.1-2017
+ */
+#define EBADMSG         9
+/**
+ * Device or resource busy.
+ * @since POSIX.1-2017
+ */
+#define EBUSY           10
+/**
+ * Operation canceled.
+ * @since POSIX.1-2017
+ */
+#define ECANCELED       11
+/**
+ * No child processes.
+ * @since POSIX.1-2017
+ */
+#define ECHILD          12
+/**
+ * Connection aborted.
+ * @since POSIX.1-2017
+ */
+#define ECONNABORTED    13
+/**
+ * Connection refused.
+ * POSIX.1-2017
+ */
+#define ECONNREFUSED    14
+/**
+ * Connection reset.
+ * @since POSIX.1-2017
+ */
+#define ECONNRESET      15
+/**
+ * Resource deadlock would occur.
+ * @since POSIX.1-2017
+ */
+#define EDEADLK         16
+/**
+ * Destination address required.
+ * @since POSIX.1-2017
+ */
+#define EDESTADDRREQ    17
+/**
+ * Mathematics argument out of domain of function.
+ * @since POSIX.1-2017
+ */
+#define EDOM            18
+/**
+ * Disk quota exceeded.
+ * This is defined as reserved in POSIX but it doesn't give a meaning for the
+ * error. The reason is likely that while the error is common the standard
+ * doesn't define disk quotas.
+ * @since POSIX.1-2017
+ */
+#define EDQUOT          19
+/**
+ * File exists.
+ * @since POSIX.1-2017
+ */
+#define EEXIST          20
+/**
+ * Bad address.
+ * @since POSIX.1-2017
+ */
+#define EFAULT          21
+/**
+ * File too large.
+ * @since POSIX.1-2017
+ */
+#define EFBIG           22
+/**
+ * Host is unreachable.
+ * @since POSIX.1-2017
+ */
+#define EHOSTUNREACH    23
+/**
+ * Identifier removed.
+ * @since POSIX.1-2017
+ */
+#define EIDRM           24
+/**
+ * Illegal byte sequence.
+ * @since POSIX.1-2017
+ */
+#define EILSEQ          25
+/**
+ * Operation in progress.
+ * @since POSIX.1-2017
+ */
+#define EINPROGRESS     26
+/**
+ * Interrupted function.
+ * @since POSIX.1-2017
+ */
+#define EINTR           27
+/**
+ * Invalid argument.
+ * @since POSIX.1-2017
+ */
+#define EINVAL          28
+/**
+ * I/O error.
+ * @since POSIX.1-2017
+ */
+#define EIO             29
+/**
+ * Socket is connected.
+ * @since POSIX.1-2017
+ */
+#define EISCONN         30
+/**
+ * Is a directory.
+ * @since POSIX.1-2017
+ */
+#define EISDIR          31
+/**
+ * Too many levels of symbolic links.
+ * @since POSIX.1-2017
+ */
+#define ELOOP           32
+/**
+ * File descriptor value too large.
+ * @since POSIX.1-2017
+ */
+#define EMFILE          33
+/**
+ * Too many links.
+ * @since POSIX.1-2017
+ */
+#define EMLINK          34
+/**
+ * Message too large.
+ * @since POSIX.1-2017
+ */
+#define EMSGSIZE        35
+/**
+ * Multihop attempted.
+ * Marked as reserved in POSIX.
+ * @since POSIX.1-2017
+ */
+#define EMULTIHOP       36
+/**
+ * Filename too long.
+ * @since POSIX.1-2017
+ */
+#define ENAMETOOLONG    37
+/**
+ * Network is down.
+ * @since POSIX.1-2017
+ */
+#define ENETDOWN        38
+/**
+ * Connection aborted by network.
+ * @since POSIX.1-2017
+ */
+#define ENETRESET       39
+/**
+ * Network unreachable.
+ * @since POSIX.1-2017
+ */
+#define ENETUNREACH     40
+/**
+ * Too many files open in system.
+ * @since POSIX.1-2017
+ */
+#define ENFILE          41
+/**
+ * No buffer space available.
+ * @since POSIX.1-2017
+ */
+#define ENOBUFS         42
+/**
+ * No such device.
+ * @since POSIX.1-2017
+ */
+#define ENODEV          44
+/**
+ * No such file or directory.
+ * @since POSIX.1-2017
+ */
+#define ENOENT          45
+/**
+ * Executable file format error.
+ * @since POSIX.1-2017
+ */
+#define ENOEXEC         46
+/**
+ * No locks available.
+ * @since POSIX.1-2017
+ */
+#define ENOLCK          47
+/**
+ * Link has been severed.
+ * Marked as reserved in POSIX.
+ * @since POSIX.1-2017
+ */
+#define ENOLINK         48
+/**
+ * Not enough space.
+ * @since POSIX.1-2017
+ */
+#define ENOMEM          49
+/**
+ * No message of the desired type.
+ * @since POSIX.1-2017
+ */
+#define ENOMSG          50
+/**
+ * Protocol not available.
+ * @since POSIX.1-2017
+ */
+#define ENOPROTOOPT     51
+/**
+ * No space left on device.
+ * @since POSIX.1-2017
+ */
+#define ENOSPC          52
+/**
+ * Functionality not supported.
+ * @since POSIX.1-2017
+ */
+#define ENOSYS          55
+/**
+ * The socket is not connected.
+ * @since POSIX.1-2017
+ */
+#define ENOTCONN        56
+/**
+ * Not a directory or a symbolic link to a directory.
+ * @since POSIX.1-2017
+ */
+#define ENOTDIR         57
+/**
+ * Directory not empty.
+ * @since POSIX.1-2017
+ */
+#define ENOTEMPTY       58
+/**
+ * State not recoverable.
+ * @since POSIX.1-2017
+ */
+#define ENOTRECOVERABLE 59
+/**
+ * Not a socket.
+ * @since POSIX.1-2017
+ */
+#define ENOTSOCK        60
+/**
+ * Not supported.
+ * @since POSIX.1-2017
+ */
+#define ENOTSUP         61
+/**
+ * Inappropriate I/O control operation.
+ * @since POSIX.1-2017
+ */
+#define ENOTTY          62
+/**
+ * No such device or address.
+ * @since POSIX.1-2017
+ */
+#define ENXIO           63
+/**
+ * Operation not supported on socket.
+ * Same as ENOTSUP.
+ * @since POSIX.1-2017
+ */
+#define EOPNOTSUPP      ENOTSUP
+/**
+ * Value too large to be stored in data type.
+ * @since POSIX.1-2017
+ */
+#define EOVERFLOW       64
+/**
+ * Previous owner died.
+ * @since POSIX.1-2017
+ */
+#define EOWNERDEAD      66
+/**
+ * Operation not permitted.
+ * @since POSIX.1-2017
+ */
+#define EPERM           67
+/**
+ * Broken pipe.
+ * @since POSIX.1-2017
+ */
+#define EPIPE           68
+/**
+ * Protocol error.
+ * @since POSIX.1-2017
+ */
+#define EPROTO          69
+/**
+ * Protocol not supported.
+ * @since POSIX.1-2017
+ */
+#define EPROTONOSUPPORT 70
+/**
+ * Protocol wrong type for socket.
+ * @since POSIX.1-2017
+ */
+#define EPROTOTYPE      71
+/**
+ * Result too large.
+ * @since POSIX.1-2017
+ */
+#define ERANGE          72
+/**
+ * Read-only file system.
+ * @since POSIX.1-2017
+ */
+#define EROFS           73
+/**
+ * Invalid seek.
+ * @since POSIX.1-2017
+ */
+#define ESPIPE          74
+/**
+ * No such process.
+ * @since POSIX.1-2017
+ */
+#define ESRCH           75
+/**
+ * Stale file handle.
+ * Marked as reserved in POSIX.
+ * @since POSIX.1-2017
+ */
+#define ESTALE          76
+/**
+ * Connection timed out.
+ * @since POSIX.1-2017
+ */
+#define ETIMEDOUT       78
+/**
+ * Text file busy.
+ * @since POSIX.1-2017
+ */
+#define ETXTBSY         79
+/**
+ * Operation would block.
+ * @since POSIX.1-2017
+ */
+#define EWOULDBLOCK     EAGAIN
+/**
+ * Cross-device link or Improper link.
+ * @since POSIX.1-2017
+ */
+#define EXDEV           80
+/**
+ * Block device required.
+ */
+#define ENOTBLK         81
 
 #ifndef KERNEL_INTERNAL
 /**
