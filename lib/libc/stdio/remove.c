43a010cc3991a6c375967b25cc424043b2210105
Olli Vanhoja <olli.vanhoja@gmail.com>
2020-07-21 23:32:03 +0200

    Cleanup

--- lib/libc/stdio/remove.c
+++ lib/libc/stdio/remove.c
@@ -1,48 +1,25 @@
-/* $Id$ */
-
 /* remove( const char * )
-
-   This file is part of the Public Domain C Library (PDCLib).
-   Permission is granted to use, modify, and / or redistribute at will.
-*/
-
-/* This is an example implementation of remove() fit for use with POSIX kernels.
-*/
+ *
+ * This file is part of the Public Domain C Library (PDCLib).
+ * Permission is granted to use, modify, and / or redistribute at will.
+ */
 
 #include <stdio.h>
-
-#ifndef REGTEST
-#include <sys/_PDCLIB_io.h>
 #include <string.h>
+#include <sys/_PDCLIB_io.h>
+#include <unistd.h>
 
 extern FILE * _PDCLIB_filelist;
 
-extern int unlink( const char * pathname );
-
-int remove( const char * pathname )
+int remove(const char * pathname)
 {
     FILE * current = _PDCLIB_filelist;
-    while ( current != NULL )
-    {
-        if ( ( current->filename != NULL ) && ( strcmp( current->filename, pathname ) == 0 ) )
-        {
+    while (current != NULL) {
+        if ((current->filename != NULL) &&
+            (strcmp(current->filename, pathname) == 0)) {
             return EOF;
         }
         current = current->next;
     }
-    return unlink( pathname );
+    return unlink(pathname);
 }
-
-#endif
-
-#ifdef TEST
-#include <_PDCLIB_test.h>
-
-int main( void )
-{
-    /* Testing covered by ftell.c (and several others) */
-    return TEST_RESULTS;
-}
-
-#endif
-
